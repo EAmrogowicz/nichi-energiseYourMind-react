886089dc103dab9641563599afa9316a4e87b803
Alexandra Batrak <4528953+alexandrabatrak@users.noreply.github.com>
2023-03-22 17:18:19 +0000

    Bg change

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,48 +1,60 @@
 import { Link } from "react-router-dom";
-import { AppBar, Box, Toolbar, Button } from "@mui/material";
+import { AppBar, Box, Toolbar } from "@mui/material";
 import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
 import HomeIcon from "@mui/icons-material/Home";
 import PersonIcon from "@mui/icons-material/Person";
+import SettingsIcon from "@mui/icons-material/Settings";
 
 export default function Nav() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar
-        position='fixed'
-        sx={{ "@media (max-width:768px)": { top: "auto", bottom: 0 } }}>
-        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-          <Box sx={{ gap: "1rem" }}>
-            <Link to='/'>
-              <HomeIcon
-                size='large'
-                edge='start'
-                color='inherit'
-                aria-label='menu'
-                sx={{ mr: 2 }}
-              />
-            </Link>
-            <Link to='/mood-tracker'>
-              <EmojiEmotionsIcon
-                size='large'
-                edge='start'
-                color='inherit'
-                aria-label='menu'
-                sx={{ mr: 2 }}
-              />
-            </Link>
-            <Link to='/user-login'>
-              <PersonIcon
-                size='large'
-                edge='start'
-                color='inherit'
-                aria-label='menu'
-                sx={{ mr: 2 }}
-              />
-            </Link>
-          </Box>
-          <Button color='inherit'>Settings</Button>
-        </Toolbar>
-      </AppBar>
+      <nav>
+        <AppBar
+          position='fixed'
+          sx={{
+            "@media (max-width:768px)": { top: "auto", bottom: 0 },
+            background: "white",
+          }}>
+          <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+            <Box sx={{ gap: "1rem" }}>
+              <Link to='/'>
+                <HomeIcon
+                  size='large'
+                  edge='start'
+                  color='inherit'
+                  aria-label='home'
+                  sx={{ mr: 2 }}
+                />
+              </Link>
+              <Link to='/mood-tracker'>
+                <EmojiEmotionsIcon
+                  size='large'
+                  edge='start'
+                  color='inherit'
+                  aria-label='mood'
+                  sx={{ mr: 2 }}
+                />
+              </Link>
+              <Link to='/user-login'>
+                <PersonIcon
+                  size='large'
+                  edge='start'
+                  color='inherit'
+                  aria-label='user'
+                  sx={{ mr: 2 }}
+                />
+              </Link>
+            </Box>
+            <SettingsIcon
+              size='large'
+              edge='start'
+              color='inherit'
+              aria-label='settings'
+              sx={{ mr: 2 }}
+            />
+          </Toolbar>
+        </AppBar>
+      </nav>
     </Box>
   );
 }
