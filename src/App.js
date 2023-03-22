343972f92339dedb4a01d9f75878d93140a1890c
Ewelina Amrogowicz <ewelina.amrogowicz@gmail.com>
2023-03-22 12:47:26 +0000

    import parameter to override mui as needed

--- src/App.js
+++ src/App.js
@@ -4,29 +4,32 @@ import {
   Routes,
   // useLocation,
 } from "react-router-dom";
-import Start from "./pages/Start";
+import Home from "./pages/Home";
 import MoodTracker from "./pages/MoodTracker";
 import UserLogin from "./pages/UserLogin";
 import Nav from "./components/Nav/Nav";
 import Footer from "./components/Footer/Footer";
+import { StyledEngineProvider } from "@mui/material/styles";
 
 export default function App() {
   // const location = useLocation();
 
   // should default page be user dashboard or start?
   return (
-    <Router>
-      <Nav />
-      <main>
-        <Routes
-        // key={location.path} location={location}
-        >
-          <Route exact path='/' element={<Start />} />
-          <Route path='/mood-tracker' element={<MoodTracker />} />
-          <Route path='/user-login' element={<UserLogin />} />
-        </Routes>
-      </main>
-      <Footer />
-    </Router>
+    <StyledEngineProvider injectFirst>
+      <Router>
+        <Nav />
+        <main>
+          <Routes
+          // key={location.path} location={location}
+          >
+            <Route path="/" element={<Home />} />
+            <Route path="/mood-tracker" element={<MoodTracker />} />
+            <Route path="/user-login" element={<UserLogin />} />
+          </Routes>
+        </main>
+        <Footer />
+      </Router>
+    </StyledEngineProvider>
   );
 }
