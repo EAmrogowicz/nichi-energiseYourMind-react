fafbe6e13a9e7a558185db3ef0079388823df112
Ewelina Amrogowicz <ewelina.amrogowicz@gmail.com>
2023-03-23 16:57:16 +0000

    remove unnecessary cards

--- src/components/Card/CardWrapper.js
+++
@@ -1,5 +0,0 @@
-import { Box } from "@mui/material";
-
-export default function CardWrapper({ children }) {
-  return <Box sx={{ width: "100%", margin: "auto" }}>{children}</Box>;
-}
--- src/components/StandardCard.js
+++ src/components/StandardCard.js
@@ -2,10 +2,10 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import { CardActionArea } from "@mui/material";
-import Heading5 from "../Typography/Heading5";
-import ParagraphLg from "../Typography/ParagraphLg";
+import Heading5 from "./Typography/Heading5";
+import ParagraphLg from "./Typography/ParagraphLg";
 
-export default function ActionAreaCard({
+export default function StandardCard({
   classCss,
   height,
   title,
@@ -13,13 +13,13 @@ export default function ActionAreaCard({
   imglink,
 }) {
   return (
-    <Card sx={{ maxWidth: 620, minWidth: 300 }} className={classCss}>
+    <Card sx={{ maxWidth: 620, minWidth: 240 }} className={classCss}>
       <CardActionArea>
         <CardMedia
           component="img"
           height={height}
           image={imglink}
-          alt="meditate"
+          alt={title}
         />
         <CardContent>
           <Heading5 text={title} classCss={"m-bottom"} />
