5513fbd4ac819ffd582c461f84f8dcfa1d4c3003
Ewelina Amrogowicz <ewelina.amrogowicz@gmail.com>
2023-03-24 18:19:14 +0000

    dark mode

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,12 +4,74 @@ import AddReactionOutlinedIcon from "@mui/icons-material/AddReactionOutlined";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 // import SettingsIcon from "@mui/icons-material/Settings";
-
 import Typography from "@mui/material/Typography";
+import { useState, useEffect } from "react";
+import { styled } from "@mui/material/styles";
+import Switch from "@mui/material/Switch";
+import FormControlLabel from "@mui/material/FormControlLabel";
+
+const MaterialUISwitch = styled(Switch)(({ theme }) => ({
+  width: 62,
+  height: 34,
+  padding: 7,
+  "& .MuiSwitch-switchBase": {
+    margin: 1,
+    padding: 0,
+    transform: "translateX(6px)",
+    "&.Mui-checked": {
+      color: "#fff",
+      transform: "translateX(22px)",
+      "& .MuiSwitch-thumb:before": {
+        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+          "#fff"
+        )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
+      },
+      "& + .MuiSwitch-track": {
+        opacity: 1,
+        backgroundColor: theme.palette.mode === "dark" ? "#e6eaef" : "white",
+      },
+    },
+  },
+  "& .MuiSwitch-thumb": {
+    backgroundColor: theme.palette.mode === "dark" ? "#718493" : "#d19793",
+    width: 32,
+    height: 32,
+    "&:before": {
+      content: "''",
+      position: "absolute",
+      width: "100%",
+      height: "100%",
+      left: 0,
+      top: 0,
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "center",
+      backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+        "#fff"
+      )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
+    },
+  },
+  "& .MuiSwitch-track": {
+    opacity: 1,
+    backgroundColor: theme.palette.mode === "dark" ? "#e6eaef" : "#e6eaef",
+    borderRadius: 20 / 2,
+  },
+}));
 
 export default function Nav() {
+  const [theme, setTheme] = useState("light");
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box sx={{ flexGrow: 1 }} className={`App ${theme}`}>
       <nav>
         <AppBar
           color="inherit"
@@ -22,7 +84,7 @@ export default function Nav() {
           <Toolbar
             sx={{
               display: "flex",
-              justifyContent: "space-between",
+              justifyContent: { xs: "center", md: "space-between" },
               alignItems: "center",
               ml: 2,
             }}
@@ -32,20 +94,25 @@ export default function Nav() {
               noWrap
               component="a"
               href="/"
+              className="navLogo"
               sx={{
                 mr: 1,
                 display: { xs: "none", md: "flex" },
                 fontFamily: "Vidaloka",
                 letterSpacing: ".3rem",
-                color: "white",
-                fontWeight: 700,
-                textDecoration: "none",
-                textShadow: "0px 1px 1px #35444b",
+                fontWeight: 100,
               }}
             >
               NICHI
             </Typography>
-            <Box sx={{ gap: 1 }}>
+            <Box
+              sx={{
+                gap: 1,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
               <Link to="/">
                 <HomeOutlinedIcon
                   edge="start"
@@ -73,6 +140,11 @@ export default function Nav() {
                   className="navIcon"
                 />
               </Link>
+              <FormControlLabel
+                onClick={toggleTheme}
+                control={<MaterialUISwitch sx={{ m: 1 }} defaultChecked />}
+              />
+              {/* <button onClick={toggleTheme}>Toggle Theme</button> */}
             </Box>
 
             {/* <SettingsIcon
