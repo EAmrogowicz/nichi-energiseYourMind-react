a8d037b2f541006a362d9614447dfe30c4c7a881
Ewelina Amrogowicz <ewelina.amrogowicz@gmail.com>
2023-03-24 09:34:55 +0000

    Review footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,3 +1,9 @@
 export default function Footer() {
-  return <footer></footer>;
+  return (
+    <footer>
+      <div className="footerSection">
+        <p className="footer">Copyright ©2023 by NICHI. All right reserved.</p>
+      </div>
+    </footer>
+  );
 }
