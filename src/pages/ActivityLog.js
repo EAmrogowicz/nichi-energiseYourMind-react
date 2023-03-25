cba1566495f3257e283aa35a454ed6f43f929318
Alexandra Batrak <4528953+alexandrabatrak@users.noreply.github.com>
2023-03-25 01:26:38 +0000

    Activity log + style updates

--- src/pages/ActivityLog.js
+++ src/pages/ActivityLog.js
@@ -1,21 +1,29 @@
 import { useState, useRef } from "react";
-import Calendar from "react-calendar";
+import { Link } from "react-router-dom";
 import { Box, Grid, Stack, Paper } from "@mui/material";
 import PageContainer from "../components/PageContainer";
-import ParagraphLg from "../components/Typography/ParagraphLg";
+import SubHeading from "../components/Typography/SubHeading";
 import Heading4 from "../components/Typography/Heading4";
+import ParagraphLg from "../components/Typography/ParagraphLg";
+import Calendar from "react-calendar";
+import IconBtn from "../components/Button/IconBtn";
 import MoodLogs from "../components/Mood/MoodLog";
-import { moods } from "../components/Mood/Moods";
 import MoodGrid from "../components/Mood/MoodGrid";
 import MoodMost from "../components/Mood/MoodMost";
-import SubHeading from "../components/Typography/SubHeading";
+import { moods } from "../components/Mood/Moods";
 
 export default function ActivityLog() {
   const userData = JSON.parse(localStorage.getItem("userData"));
   const moodData = userData.mood;
+  const meditationData = userData?.meditation;
   const [filteredMoodData, setFilteredMoodData] = useState([]);
   const [selectedDate, setSelectedDate] = useState("");
   const calendarRef = useRef();
+  const minDate =
+    moodData &&
+    new Date(
+      Math.min(...moodData.map((moodRecord) => new Date(moodRecord.time)))
+    );
 
   function getStreak(moodData) {
     let [streak, currStreak] = [0, 0];
@@ -52,130 +60,164 @@ export default function ActivityLog() {
   }
 
   return (
-    <PageContainer size={"lg"} stackDisable={true}>
-      <Box sx={{ m: "1.2rem auto 2.4rem" }}>
-        <SubHeading text={"Your activity"} />
-      </Box>
-      <Grid container columns={12} width={"100%"}>
-        <Grid item xs={12} sm='auto' md={5} sx={{ mx: "auto" }}>
-          <Stack>
-            <Box sx={{ m: "1.2rem auto" }}>
-              <Calendar
-                ref={calendarRef}
-                onClickDay={handleDateSelect}
-                maxDate={new Date()}
-                tileClassName={dayClassName}
-                minDate={
-                  new Date(
-                    Math.min(
-                      ...moodData.map((moodRecord) => new Date(moodRecord.time))
-                    )
-                  )
-                }
-              />
-            </Box>
-            <Paper
-              elevation={5}
-              className='paper-lg-bg'
+    <PageContainer size={"md"}>
+      {!moodData || meditationData ? (
+        <>
+          <Box sx={{ m: "1.2rem auto 2.4rem", textAlign: "center" }}>
+            <SubHeading text={"Your activity"} />
+            <ParagraphLg text={"Nothing here yet."} />
+          </Box>
+          <Box>
+            <Link to='/'>
+              <IconBtn />
+            </Link>
+          </Box>
+        </>
+      ) : (
+        <>
+          <Box sx={{ m: "1.2rem auto 2.4rem" }}>
+            <SubHeading text={"Your activity"} />
+          </Box>
+          <Box
+            sx={{
+              m: "0 !important",
+              display: "grid",
+              "@media( min-width: 900px)": {
+                gridTemplateColumns: "repeat(12, 1fr)",
+              },
+            }}>
+            <Box
               sx={{
-                m: "2.4rem auto",
-                "@media( min-width: 768px)": { p: "2.4rem" },
+                mx: "auto",
+                "@media( min-width: 900px)": { gridArea: "1/1/1/6" },
+                width: "100%",
               }}>
-              <Grid container columns={2}>
-                <Grid item xs={2} sm={1}>
-                  <Stack>
-                    <Box
-                      sx={{
-                        m: "1.2rem auto",
-                        textAlign: "center",
-                        "@media( min-width: 768px)": { my: "2.4rem" },
-                      }}>
-                      <ParagraphLg text={`Your streak is`} />
-                      <Box className='streak-box'>
-                        <span>{getStreak(moodData)}</span>
-                      </Box>
-                      <ParagraphLg text={"days"} />
-                    </Box>
-                  </Stack>
-                </Grid>
-                <Grid
-                  item
-                  xs={2}
-                  sm={1}
+              <Stack
+                sx={{
+                  "@media(min-width:900px)": {
+                    flexDirection: "column-reverse",
+                  },
+                }}>
+                <Paper
+                  elevation={5}
+                  className='paper-lg-bg'
                   sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
+                    "@media( min-width: 900px)": { p: "2.4rem" },
                   }}>
-                  <Stack>
-                    <Box sx={{ "@media( max-width: 768px)": { mb: "2.4rem" } }}>
-                      <MoodMost />
-                    </Box>
-                  </Stack>
-                </Grid>
-              </Grid>
-            </Paper>
-          </Stack>
-        </Grid>
-        <Grid item xs={12} sm='auto' md={7} sx={{ mx: "auto" }}>
-          <Stack>
-            {selectedDate && filteredMoodData.length > 0 ? (
-              <>
-                <Heading4
-                  text={`Entries for ${new Date(selectedDate).toLocaleString(
-                    "en-GB",
-                    {
-                      dateStyle: "short",
-                      timeZone: "Europe/London",
-                    }
-                  )}`}
-                />
-                <Box
-                  className='moodlog-wrapper'
-                  sx={{
-                    p: "2.4rem",
-                    my: "1.2rem",
-                    "@media (min-width: 768px)": { mx: "2.4rem" },
-                  }}>
-                  {filteredMoodData
-                    .sort((a, b) => new Date(b.time) - new Date(a.time))
-                    .map((moodRecord) => {
-                      const matchingMood = moods.find(
-                        (mood) => mood.description === moodRecord.mood
-                      );
-                      const MoodIconRecord = matchingMood?.icon || null;
-                      return (
+                  <Grid container columns={2}>
+                    <Grid item xs={2} sm={1}>
+                      <Stack>
                         <Box
-                          className='moodlog-item'
                           sx={{
-                            p: "1.2rem",
-                          }}
-                          key={moodRecord.time}>
-                          <MoodGrid
-                            icon={MoodIconRecord}
-                            desc={matchingMood?.description || null}
-                            time={new Date(moodRecord.time).toLocaleString(
-                              "en-GB",
-                              {
-                                timeStyle: "short",
-                                timeZone: "Europe/London",
-                              }
-                            )}
-                            notes={moodRecord.notes}
-                            iconboxsize={20}
-                            iconsize={"3rem"}
-                          />
+                            m: "1.2rem auto",
+                            textAlign: "center",
+                            "@media( min-width: 900px)": { my: "2.4rem" },
+                          }}>
+                          <ParagraphLg text={`Your streak is`} />
+                          <Box className='streak-box'>
+                            <span>{getStreak(moodData)}</span>
+                          </Box>
+                          <ParagraphLg text={"days"} />
                         </Box>
-                      );
-                    })}
+                      </Stack>
+                    </Grid>
+                    <Grid
+                      item
+                      xs={2}
+                      sm={1}
+                      sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}>
+                      <Stack>
+                        <Box
+                          sx={{
+                            "@media( max-width: 900px)": { mb: "2.4rem" },
+                          }}>
+                          <MoodMost />
+                        </Box>
+                      </Stack>
+                    </Grid>
+                  </Grid>
+                </Paper>
+                <Box sx={{ m: "2.4rem auto" }}>
+                  <Calendar
+                    ref={calendarRef}
+                    onClickDay={handleDateSelect}
+                    maxDate={new Date()}
+                    tileClassName={dayClassName}
+                    minDate={minDate}
+                  />
                 </Box>
-              </>
-            ) : (
-              <MoodLogs />
-            )}
-          </Stack>
-        </Grid>
-      </Grid>
+              </Stack>
+            </Box>
+            <Box
+              sx={{
+                mx: "auto",
+                "@media( min-width: 900px)": { gridArea: "1/6/1/13" },
+                width: "100%",
+              }}>
+              <Stack>
+                {selectedDate && filteredMoodData.length > 0 ? (
+                  <>
+                    <Heading4
+                      text={`Entries for ${new Date(
+                        selectedDate
+                      ).toLocaleString("en-GB", {
+                        dateStyle: "short",
+                        timeZone: "Europe/London",
+                      })}`}
+                    />
+                    <Box
+                      className='moodlog-wrapper'
+                      sx={{
+                        width: "100%",
+                        p: "2.4rem",
+                        my: "1.2rem",
+                        "@media (min-width: 900px)": { mx: "2.4rem" },
+                      }}>
+                      {filteredMoodData
+                        .sort((a, b) => new Date(b.time) - new Date(a.time))
+                        .map((moodRecord) => {
+                          const matchingMood = moods.find(
+                            (mood) => mood.description === moodRecord.mood
+                          );
+                          const MoodIconRecord = matchingMood?.icon || null;
+                          return (
+                            <Box
+                              className='moodlog-item'
+                              sx={{
+                                p: "1.2rem",
+                              }}
+                              key={moodRecord.time}>
+                              <MoodGrid
+                                icon={MoodIconRecord}
+                                desc={matchingMood?.description || null}
+                                time={new Date(moodRecord.time).toLocaleString(
+                                  "en-GB",
+                                  {
+                                    timeStyle: "short",
+                                    timeZone: "Europe/London",
+                                  }
+                                )}
+                                notes={moodRecord.notes}
+                                iconboxsize={20}
+                                iconsize={"3rem"}
+                              />
+                            </Box>
+                          );
+                        })}
+                    </Box>
+                  </>
+                ) : (
+                  <MoodLogs />
+                )}
+              </Stack>
+            </Box>
+          </Box>
+        </>
+      )}
     </PageContainer>
   );
 }
