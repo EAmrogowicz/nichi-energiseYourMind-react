6a4fb5505cf001864674b48a3abf8731c6a85722
Alexandra Batrak <4528953+alexandrabatrak@users.noreply.github.com>
2023-03-26 17:44:07 +0100

    -Unrelated to branch - add meditations.js

---
+++ src/components/Mood/Meditations.js
@@ -0,0 +1,20 @@
+export const meditations = [
+  {
+    meditation: "MeditateBreath",
+    meditationImg:
+      "https://images.unsplash.com/photo-1601779144646-5e6a43c5d615?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG1lZGl0YXRlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
+    suitableMood: ["Happy", "Good", "OK"],
+  },
+  {
+    meditation: "MeditateBodyScan",
+    meditationImg:
+      "https://images.unsplash.com/photo-1602144564887-e2be90e0ab11?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
+    suitableMood: ["Neutral", "Sad", "Awful"],
+  },
+  {
+    meditation: "MeditateReflect",
+    meditationImg:
+      "https://images.unsplash.com/photo-1601779144646-5e6a43c5d615?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG1lZGl0YXRlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60",
+    suitableMood: ["Neutral", "OK"],
+  },
+];
