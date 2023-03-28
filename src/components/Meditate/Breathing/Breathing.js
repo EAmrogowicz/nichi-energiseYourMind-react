a44082e2773309558e80457336bfa61df9481db5
HaveTime.DrinkTea <havetimedrinktea@gmail.com>
2023-03-28 10:53:27 +0100

    added comments to the playpause function for all 3 meditation components.

--- src/components/Meditate/Breathing/Breathing.js
+++ src/components/Meditate/Breathing/Breathing.js
@@ -71,9 +71,9 @@ export default function MeditateBreath() {
   const [isEnded, setIsEnded] = useState(false);
 
   //--PW To toggle between playing or pausing the meditation timer
-  //--PW to solve the React warning "Function makes the dependencies of useEffect Hook change on every render":
-  //-- (1) cannot move playPause() into useEffect as it is needed inside the JSX when playPause button is clicked
-  //-- (2) so instead wrap the playPause() function definition in a useCallback hook which will hopefully return a memoized function whose reference will only change if something in the hook's dependency array changes. 
+  //--PW There are 2 ways to solve the React warning "Function makes the dependencies of useEffect Hook change on every render":
+  //-- (Option 1) cannot move playPause() into useEffect as it is needed inside the JSX when playPause button is clicked
+  //-- (Option 2) so instead wrap the playPause() function definition in a useCallback hook which will hopefully return a memoized function whose reference will only change if something in the hook's dependency array changes. 
 
   const playPause = useCallback(() => {
     setIsActive(!isActive);
